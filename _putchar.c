c2a6fea6add2d140eb49a268b31696c1c6117f26
Pascalchinedu <pascalchinedu2000@gmail.com>
2022-04-18 15:12:09 -0700

    printf Project

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,4 @@
-#include <unistd.h>
-<<<<<<< HEAD
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,18 +6,6 @@
  * Return: 1 on SUCCESS,
  *	   -1 on error and errno is set appropriately.
  */
-=======
-#include <stdlib.h>
-#include <stdio.h>
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-
->>>>>>> 0f374be989095f8521b7f7ca6593d0b03f10b9e2
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
